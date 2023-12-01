6302d2209ed99d07077436b6699cea8d7c161163
Milan Hauth <milahu@gmail.com>
2023-12-01 11:46:54 +0100

    parse missing zip filename from nfo file

--- add-files.sh
+++ add-files.sh
@@ -40,10 +40,77 @@ for file_path in "${files[@]}"; do
 
   old_name="$(basename "$file_path")"
 
+  echo
+
   if ! [ -e "$file_path" ]; then
     echo "no such file: $file_path"
     continue
   fi
+
+  echo "input file: $file_path"
+
+  file_type="$(file -b --mime-type "$file_path")"
+  case "$file_type" in
+    application/zip)
+      # 12345.zip
+
+      # allow lossy filenames like 12345.zip
+      # wontfix: the *.nfo files contain only a truncated version of the filename
+      # because the values are truncated after 58 bytes
+      # we tolerate a missing filename extension ".zip"
+      # so if the zip filename is longer than 62 bytes, the zip filename is lost
+      # and we have to re-download the zip file, to get the original zip filename
+
+      #echo "found zip file: $file_path"
+      nfo_filename="$(unzip -l -q -q "$file_path" | grep '\.nfo$' | awk '{ print $4 }')"
+      if [ -z "$nfo_filename" ]; then
+        echo "not found *.nfo file in zipfile: $file_path"
+        continue
+      fi
+      #echo "found nfo file: $nfo_filename"
+      # unpack the nfo file
+      tempdir=$(mktemp -d --suffix=-new-subs-repo-add-files -p /run/user/$(id -u)/)
+      unzip -q -q "$file_path" "$nfo_filename" -d "$tempdir"
+      nfo_path="$tempdir/$nfo_filename"
+      # parse zip filename from nfo
+      maybe_zip_name="$(grep '^.  Filename       : ' "$nfo_path" | head -n1 | cut -c22-79 | tr -d ' ')"
+      #echo "maybe zip name: $maybe_zip_name"
+      if [[ "$maybe_zip_name" == *".zip" ]]; then
+        echo "nfo: found zip name: $maybe_zip_name"
+        old_name="$maybe_zip_name"
+      else
+        #echo "nfo: found truncated zip name: $maybe_zip_name"
+        maybe_zip_basename="$(
+          echo "$maybe_zip_name" |
+          grep -o -E "^.*\.\([0-9]+\)\.[a-z]{3}\.[0-9]+cd\.\(([0-9]+)\)" || true
+        )"
+        #echo "nfo: maybe zip basename: $maybe_zip_basename"
+        if [ -n "$maybe_zip_basename" ]; then
+          # use a slightly truncated zip filename
+          # the ".zip" extension can be truncated
+          # example:
+          # encounters.s01.e01.episode.1.1.(2023).hrv.1cd.(9756714).z
+          old_name="$maybe_zip_basename.zip"
+          echo "nfo: found zip name: $old_name"
+        else
+          echo "nfo: not using truncated zip filename: $maybe_zip_name"
+        fi
+      fi
+      # cleanup
+      rm -rf "$tempdir"
+      ;;
+    inode/x-empty)
+      # 12345.not-found
+      if ! echo "$old_name" | grep -q -E "^[0-9]+\.not-found$"; then
+        echo "ignoring non-subtitle file: $file_path"
+        continue
+      fi
+      ;;
+    *)
+      echo "ignoring non-subtitle file: $file_path"
+      continue
+      ;;
+  esac
 
   if ! echo "$old_name" | grep -q -E "^([0-9]+\.(.*\.[0-9]+cd\.zip|not-found)|.*\.[0-9]+cd\.\(([0-9]+)\)\.zip)$"; then
     echo "ignoring non-subtitle file: $file_path"
@@ -68,7 +135,7 @@ for file_path in "${files[@]}"; do
   num=$(echo "$new_name" | cut -d. -f1)
 
   if grep -q "^$num." files.txt; then
-    echo "ignoring file: $new_name"
+    echo "output file exists: $new_name"
     mv -v "$file_path" trash/ || rm -v "$file_path"
     continue
   fi

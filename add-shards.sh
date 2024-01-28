3d0fd550dc67e64c0479e5b413e896d15e735ad9
Milan Hauth <milahu@gmail.com>
2024-01-28 20:43:12 +0100

    up add-shards.sh

--- add-shards.sh
+++ add-shards.sh
@@ -2,18 +2,4 @@
 
 cd "$(dirname "$0")"
 
-for db in ../new-subs-shards/shards/*.db
-do
-
-  n=$(basename "$db")
-  s=$(echo "$n" | sed -E 's/^([0-9]*)([0-9]{2})xxx\.db$/\1xxxxx/')
-
-  if [ "$s" = "xxxxx" ]; then s="0xxxxx"; fi
-
-  if ! [ -e "shards/$s" ]; then mkdir -v -p "shards/$s"; fi
-
-  dst="shards/$s/$n"
-
-  if ! [ -e "$dst" ]; then ln -v "$db" "$dst"; fi
-
-done
+git add . && git commit -m "add shards"

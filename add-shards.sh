287885e01397f28e32d8aa71427405ee0b99b63a
Milan Hauth <milahu@gmail.com>
2024-03-06 15:47:53 +0100

    up add-shards.sh

--- add-shards.sh
+++ add-shards.sh
@@ -2,4 +2,17 @@
 
 cd "$(dirname "$0")"
 
-git add shards/ && git commit -m "add shards"
+# add one shard per commit to allow incremental "git push"
+
+while read shard_path; do
+  echo "shard_path $shard_path"
+  shard_name=$(basename $shard_path .db)
+  echo "shard_name $shard_name"
+  #continue # dry run
+  git add "$shard_path"
+  git commit -m "add shard $shard_name"
+  sleep 1
+done < <(
+  # note: temporary files are ignored by .gitignore
+  git status shards/ --porcelain=2 | grep '^? ' | cut -c3-
+)

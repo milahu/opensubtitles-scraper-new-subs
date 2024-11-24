2e89d85672e9e7722d35f36b760217624a108156
Milan Hauth <milahu@gmail.com>
2024-11-24 10:16:26 +0100

    up add-shards.sh

--- add-shards.sh
+++ add-shards.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
+git_user_name="Milan Hauth"
+git_user_email="milahu@gmail.com"
+
 cd "$(dirname "$0")"
 
 # add one shard per commit to allow incremental "git push"
+
+# undo previous "git add"
+git restore --staged .
 
 while read shard_path; do
   echo "shard_path $shard_path"
@@ -10,7 +16,7 @@ while read shard_path; do
   echo "shard_name $shard_name"
   #continue # dry run
   git add "$shard_path"
-  git commit -m "add shard $shard_name"
+  git -c user.name="$git_user_name" -c user.email="$git_user_email" commit -m "add shard $shard_name"
   sleep 1
 done < <(
   # note: temporary files are ignored by .gitignore

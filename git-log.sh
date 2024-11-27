05482cbcd490a2825ad76c22d0b83debe163a864
Milan Hauth <milahu@gmail.com>
2024-11-27 15:32:38 +0100

    add git-log.sh

---
+++ git-log.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# show git log of all branches, sorted by commit date
+
+main_branch=main
+
+branches=(
+  $main_branch
+  $(
+    git branch --format="%(refname:short)" |
+    grep -E '^shards-[0-9]+xxxxx$'
+  )
+)
+
+for branch in "${branches[@]}"; do
+  git log --format="%cI $branch %h %s" "$branch" | head -n100
+done |
+sort -r -k1 |
+head -n20

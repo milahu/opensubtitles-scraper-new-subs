ade078adaf0db4dafc77d7669c3b63a3f9f2a118
Milan Hauth <milahu@gmail.com>
2025-08-05 18:14:41 +0200

    fix mount-branches.sh

--- mount-branches.sh
+++ mount-branches.sh
@@ -1,29 +1,35 @@
 #!/usr/bin/env bash
 
+set -ex
+
 cd "$(dirname "$0")"
 
 declare -A worktree_set
-for dir in .git/worktrees/*; do
-  worktree="$(cat "$dir"/gitdir)"
-  # remove "/.git" suffix
-  worktree="${worktree:0: -5}"
-  worktree="$(realpath "$worktree")"
-  worktree_set["$worktree"]=1
-done
+if [ -e .git/worktrees ]; then
+  for dir in .git/worktrees/*; do
+    worktree="$(cat "$dir"/gitdir)"
+    # remove "/.git" suffix
+    worktree="${worktree:0: -5}"
+    worktree="$(realpath "$worktree")"
+    worktree_set["$worktree"]=1
+  done
+fi
 
 # mount all branches
 while read branch; do
   # a: shards-102xxxxx
   # b: shards/102xxxxx
-  dir=${branch/-/"/"}
-  worktree="$(realpath "$dir")"
-  if [ "${worktree_set[$worktree]}" = 1 ]; then
+  branch_name=${branch##*/} # "origin/shards-102xxxxx" -> "shards-102xxxxx"
+  dir=${branch_name/-/"/"}
+  worktree="$(realpath -m "$dir")" # get absolute path
+  if [ "${worktree_set[$worktree]}" = 1 ]; then # this breaks "set -u"
     # already mounted
     continue
   fi
   # "worktree add" fails if worktree exists
   git worktree add $dir $branch || continue
 done < <(
-  git branch --format="%(refname:short)" |
-  grep -E '^shards-[0-9]+xxxxx$'
+  # note: also match "origin/shards-102xxxxx" etc
+  git branch --all --format="%(refname:short)" |
+  grep -E -e '^shards-[0-9]+xxxxx$' -e '/shards-[0-9]+xxxxx$'
 )

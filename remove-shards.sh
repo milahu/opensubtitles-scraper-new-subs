57b60f903c3f5b789dbc181c94a5dd1883407732
Milan Hauth <milahu@gmail.com>
2025-08-06 11:53:54 +0200

    fix remove-shards.sh

--- remove-shards.sh
+++ remove-shards.sh
@@ -10,19 +10,37 @@ if [ $# != 1 ]; then
   exit 1
 fi
 
-if ! [ -d "$1" ] || ! echo "$1" | grep -q -E '^shards/[0-9]+xxxxx/?$'; then
-  echo "error: bad argument: ${1@Q}"
-  echo "argv[0] must be path to shards dir, for example shards/12xxxxx/"
-  exit 1
-fi
+path_list=()
+for arg in "$@"; do
+  if ! [ -d "$arg" ] || ! echo "$arg" | grep -q -E '^shards/[0-9]+xxxxx/?$'; then
+    echo "error: bad argument: ${arg@Q}"
+    echo "arg must be path to shards dir, for example shards/12xxxxx/"
+    exit 1
+  fi
+  path_list+=("$arg")
+done
 
-set -x
-
-git checkout main
-
-git branch bak-main-$(date --utc +%Y%m%dT%H%M%SZ)
-
-time \
-git-filter-repo --force --refs main --invert-paths --path "$1"
+for path in "${path_list[@]}"; do
+    # path: shards/12xxxxx/
+    git worktree remove "$path"
+    branch_name="shards-$(basename "$path")" # shards-12xxxxx
+    # shards-12xxxxx -> origin/shards-102xxxxx
+    # delete local branches
+    git branch --all --format="%(refname:short)" |
+    grep -m1 -E -e "^$branch_name$" -e "/$branch_name$" |
+    while read -r branch; do
+        echo "deleting local branch $branch"
+        if [ "$branch" = "$branch_name" ]; then
+            git branch --delete "$branch"
+        else
+            git branch --delete --remote "$branch"
+        fi
+    done
+    # delete remote branches
+    for remote in $(git remote show); do
+        echo "deleting remote branch $branch_name from remote $remote"
+        git push "$remote" --delete "$branch_name"
+    done
+done
 
 echo ok
